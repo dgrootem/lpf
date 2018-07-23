52d90528154cb0c81d7753db0f46cc07afdb152a
ibliss <ibliss@THUIS4.THUIS.NET>
2018-07-23 15:50:13 +0200

    Initial commit

---
+++ database/migrations/2018_07_23_131750_create_schools_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchoolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schools', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('naam');
+            $table->integer('lestijden_per_week');
+            $table->string('type'); //BaO ofwel BuO
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schools');
+    }
+}
