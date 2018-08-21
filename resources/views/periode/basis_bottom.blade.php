a7f3158a418fe41b30049345449ba1118e1f3d65
De Groote <michael.de.groote@gmail.com>
2018-08-21 08:41:45 +0200

    cleanup

--- resources/views/periode/basis_bottom.blade.php
+++ resources/views/periode/basis_bottom.blade.php
@@ -1,11 +1,1 @@
 
-
-
-<div class="form-row">
-  <div class="col">
-    <a class="btn btn-secondary btn-close" href="{{url('/')}}">Annuleer</a>
-  </div>
-  <div class="col">
-    <button id="mysubmit" type="submit" class="btn btn-primary float-md-right">Bewaar</button>
-  </div>
-</div>
