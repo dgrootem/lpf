f77a08d872461309a34e76a5fe45d2e0095c9bd2
dgrootem <michael.de.groote@gmail.com>
2018-09-17 19:28:08 +0200

    weekschema toevoegen werkt nu correct

--- resources/views/leerkracht/edit.blade.php
+++ resources/views/leerkracht/edit.blade.php
@@ -50,15 +50,16 @@
 <script type="text/javascript">
 $(document).ready(function () {
   $('#addWeekschema').on('click',function(){
-    var myclone = $('#weekschema1').clone();
-    var count = $("#accordion").children().length +1;
-    myclone.attr('id','weekschema'+count);
-    myclone.appendTo("#accordion");
+    var count = $("#accordion").children().length;
+    var original = $('#weekschema1');
+    var myclone =  original.clone();
+
+    count++;
+
+    original.attr('id','weekschema'+count);
+    myclone.prependTo("#accordion");
 
     $("#weekschema" + count + " > .card-header").attr('id','heading'+count);
-    $("#weekschema" + count + " > .card-header").attr('data-target','collapse'+count);
-    $("#weekschema" + count + " > .card-header").attr('aria-controls','collapse'+count);
-
     $("#weekschema" + count + " > .card-header >h5 > button").attr('id','toggleButton'+count);
 
     $("#weekschema" + count + " > #collapse1").attr('aria-labelledby','heading'+count);
@@ -66,13 +67,18 @@ $(document).ready(function () {
 
 
     $("#toggleButton"+count).html('Weekschema week '+count);
+    $("#toggleButton"+count).attr('data-target','#collapse'+count);
+    $("#toggleButton"+count).attr('aria-controls','collapse'+count);
 
-    $("#collapse "+count+" > div > div:nth-child(1) > div > div.col-9 > div > div > div.col > div:nth-child(3) > div:nth-child(3) ")
+    //$("#collapse "+count+" > div > div:nth-child(1) > div > div.col-9 > div > div > div.col > div:nth-child(3) > div:nth-child(3) ")
 
-    var items = $("#collapse "+count).find(".dagdeel");
+    var items = $("#collapse"+count).find(".dagdeel");
     //var items = document.getElementsByClassName('dagdeel');
     items.each(function (k,i){ $(this).attr('name',$( this ).attr('name').replace('W1','W'+count));});
-    
+
+    var a =$("#accordion").children().sort(function(a,b){if (a.id > b.id) return 1; else return -1;});
+    $("#accordion").append(a);
+
   });
 });
 </script>
